90667df42fd391ece691abdde962d15a64740e3c
Mykhailo Trufyn <mishatrufyn@gmail.com>
2024-05-12 14:26:24 +0300

    fix: navigate to board if invalid path

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,13 +1,15 @@
 import BoardLayout from '../components/BoardLayout';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import TaskDetailPage from '../pages/TaskDetailPage';
 
 const AppRouter = () => {
   return (
     <Routes>
+      <Route index element={<Navigate to={'/board'} />} />
       <Route path="/board" element={<BoardLayout />}>
         <Route path="card/:id" element={<TaskDetailPage />} />
       </Route>
+      <Route path="*" element={<Navigate to={'/board'} />} />
     </Routes>
   );
 };
